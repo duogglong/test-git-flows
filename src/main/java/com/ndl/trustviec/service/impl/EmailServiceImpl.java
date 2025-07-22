aad5c6e44f2d3734eb4529f21a1062087253bea4
duogglong <nduonglong02@gmail.com>
2025-07-22 22:19:11 +0700

    ILCMS-2178: email

--- src/main/java/com/ndl/trustviec/service/impl/EmailServiceImpl.java
+++ src/main/java/com/ndl/trustviec/service/impl/EmailServiceImpl.java
@@ -5,9 +5,13 @@ import com.ndl.trustviec.common.exception.CommonException;
 import com.ndl.trustviec.common.type.EmailType;
 import com.ndl.trustviec.common.type.SendEmailType;
 import com.ndl.trustviec.dto.Email;
+import com.ndl.trustviec.dto.request.OtpTransactionRequest;
 import com.ndl.trustviec.dto.request.SendEmailRequest;
+import com.ndl.trustviec.dto.response.OtpTransactionResponse;
+import com.ndl.trustviec.entity.OtpTransactionEntity;
 import com.ndl.trustviec.repository.CommonConfigRepository;
 import com.ndl.trustviec.service.EmailService;
+import com.ndl.trustviec.utils.OtpGenerator;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import jakarta.mail.internet.MimeMessage;
@@ -20,6 +24,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 @Slf4j
@@ -85,6 +91,71 @@ public class EmailServiceImpl extends BaseService implements EmailService {
                 .variables(request.getVariables())
                 .build();
         sendHtmlMail(email);
+    }
+
+    public void requestSendMail2(SendEmailRequest request) {
+        if (Objects.isNull(request) || request.isNull()) {
+            throw CommonException.create(HttpStatus.BAD_REQUEST).code(ErrorConstants.BAD_REQUEST);
+        }
+        log.info("{}: requestSendMail with request --> {}", getClass().getSimpleName(), request);
+
+        String subject, template;
+        switch (SendEmailType.getType(request.getType())) {
+            case FIRST_PASSWORD -> {
+                subject = EmailType.FIRST_PASSWORD.getSubjectVi();
+                template = EmailType.FIRST_PASSWORD.getTemplateVi();
+            }
+            default -> throw CommonException.create(HttpStatus.BAD_REQUEST).code(ErrorConstants.BAD_REQUEST);
+        }
+
+        Email email = Email.builder()
+                .mailTo(request.getMailTo())
+                .subject(subject)
+                .template(template)
+                .variables(request.getVariables())
+                .build();
+        sendHtmlMail(email);
+    }
+
+    public OtpTransactionResponse sendOtp2(OtpTransactionRequest request) {
+        try {
+            log.info("{}: sendOtp.request --> {}", getClass().getSimpleName(), request);
+            if (Objects.isNull(request)) {
+                log.warn("{}: request is null", getClass().getSimpleName());
+                throw CommonException.create(HttpStatus.BAD_REQUEST).code(ErrorConstants.BAD_REQUEST);
+            }
+            // Validate
+            if (Objects.isNull(request.getType())) {
+                log.warn("{}: type is null", getClass().getSimpleName());
+                throw CommonException.create(HttpStatus.BAD_REQUEST).code(ErrorConstants.BAD_REQUEST);
+            }
+
+            OtpTransactionEntity entity = OtpTransactionEntity.builder()
+                    .cif(request.getCif())
+                    .email(request.getEmail())
+                    .otp(OtpGenerator.generateOTP())
+                    .type(request.getType().getCode())
+                    .requestObject(request.getRequestObject())
+                    .build();
+            otpTransactionRepository.save(entity);
+
+            Map<String, Object> variables = new HashMap<>();
+            variables.put("otp", entity.getOtp());
+
+            Email email = Email.builder()
+                    .mailTo(entity.getEmail())
+                    .subject(request.getType().getSubjectVi())
+                    .template(request.getType().getTemplateVi())
+                    .variables(variables)
+                    .build();
+
+            executorService.submit(() -> emailService.sendHtmlMail(email));
+
+            return OtpTransactionResponse.builder().transactionId(entity.getId()).build();
+        } catch (Exception ex) {
+            log.error("{}: Exception when sendOtp", getClass().getSimpleName(), ex);
+            throw ex;
+        }
     }
 
 }
